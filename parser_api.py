68dddb7f1d64a264d6438cb44cf11e8c4ad71b0c
ngparas <nickgp@gmail.com>
2017-05-11 00:26:07 -0500

    added stub for the user query classifier

--- parser_api.py
+++ parser_api.py
@@ -21,6 +21,27 @@ def get_procedure():
     target_key = process.extractOne(query, available_keys)[0].replace(' ', '-')
     return json.dumps({'key': target_key, 'graph': json.loads(redis_client.get(target_key).decode('utf-8'))})
 
+@app.route('/classifier')
+def classify_text():
+    query = request.args.get('query')
+    if any([i in query for i in ['next', 'forward', 'continue', 'go on', 'keep going']]):
+        return json.dumps({'textClass': 'nav',
+                           'value': 'next'})
+    elif any([i in query for i in ['previous', 'back']]):
+        return json.dumps({'textClass': 'nav',
+                           'value': 'prev'})
+    elif any([i in query for i in ['repeat']]):
+        return json.dumps({'textClass': 'nav',
+                           'value': 'stay'})
+    elif any([i in query for i in ['yes', 'yeah', 'yah', 'sure', 'great', 'okay', 'yup']]):
+        return json.dumps({'textClass': 'answer',
+                           'value': True})
+    elif any([i in query for i in ['no', 'nope', 'nah']]):
+        return json.dumps({'textClass': 'answer',
+                           'value': False})
+    else:
+        return json.dumps({'textClass': 'question',
+                           'value': True})
 
 
 if __name__ == '__main__':
