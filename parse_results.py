298b56e1da395af86ff027c9d4878549054efd7e
Andrew Tang <andrewtang@sbcglobal.net>
2017-05-11 13:23:25 -0500

    parse_procedure returns correct parsed graph

--- parse_results.py
+++ parse_results.py
@@ -54,7 +54,7 @@ def parse_step(step_text, links, is_main_step):
             if len(parsed_conditionals.get('conditionals')) > 0:
                 parsed_procedure.append(generate_step_entry(step_text, 'conditional', links, is_main_step,
                     parsed_conditionals.get('conditionals'), parsed_conditionals.get('nonconditionals')))
-            if len(parsed_conditionals.get('nonconditionals')) > 0:
+            if len(parsed_conditionals.get('conditionals')) == 0:
                 nc = parsed_conditionals.get('nonconditionals')
                 if is_action(nc):
                     parsed_procedure.append(generate_step_entry(nc, 'action', links, is_main_step))
@@ -92,7 +92,7 @@ def parse_procedure(procedure):
         procedure_dict (Dict): A procedure scraped from webmd
 
     Return:
-        A list of nodes, where each node is a Dict
+        A dict of nodes, where each node is a Dict
     """
 
     parsed_procedure = []
@@ -100,7 +100,6 @@ def parse_procedure(procedure):
     for step in procedure.get('order'):
         if is_911(step):
             current_main_step = extract_911_clauses(step)
-            print(current_main_step)
             for substep in procedure.get('steps').get(step):
                 substep['type'] = '911-conditional-list-item'
                 current_main_step['substeps'].append(substep)
@@ -115,44 +114,47 @@ def parse_procedure(procedure):
             current_main_step = parse_step(step, [], True)
 
             for substep in procedure.get('steps').get(step):
-                current_main_step[0]['substeps'].append(parse_step(substep.get('text'), substep.get('links'), False))
+                current_main_step[0]['substeps'] += parse_step(substep.get('text'), substep.get('links'), False)
             parsed_procedure += current_main_step
 
-    return parsed_procedure
-
-def add_pointers(graph):
-    """Walk the graph to add in navigation pointers
-
-    Args:
-        graph (Dict): Graph parsed from webmd scraped results
-
-    Return:
-        A graph (Dict) with navigation pointers
-    """
-    size = max(graph.keys())
-    for i in graph.keys():
-        node_type = graph[i].get('type')
-        if node_type in ['911-conditional', 'doctor-conditional']:
-            ptr = None #TODO : how to represent end of graph
-            for j in range(i+1, size):
-                if 'item' not in graph[j].get('type'):
-                    ptr = j
-                    break
-            graph[i]['true'] = i + 1 if (i + 1 <= size) else None
-            graph[i]['false'] = ptr
-        elif node_type == 'conditional':
-            ptr = None
-            for k in range(i+2, size):
-                if graph[i].get('level') == graph[k].get('level'):
-                    ptr = k
-                    break
-            graph[i]['true'] = i + 1 if (i + 1 <= size) else None
-            graph[i]['false'] = k
-        elif node_type in ['action', 'info']:
-            graph[i]['true'] = i + 1 if (i + 1 <= size) else None
-            graph[i]['false'] = i + 1 if (i + 1 <= size) else None
-
+    graph = {}
+    for i, p in enumerate(parsed_procedure):
+        graph[i] = p
     return graph
+
+# def add_pointers(graph):
+#     """Walk the graph to add in navigation pointers
+#
+#     Args:
+#         graph (Dict): Graph parsed from webmd scraped results
+#
+#     Return:
+#         A graph (Dict) with navigation pointers
+#     """
+#     size = max(graph.keys())
+#     for i in graph.keys():
+#         node_type = graph[i].get('type')
+#         if node_type in ['911-conditional', 'doctor-conditional']:
+#             ptr = None #TODO : how to represent end of graph
+#             for j in range(i+1, size):
+#                 if 'item' not in graph[j].get('type'):
+#                     ptr = j
+#                     break
+#             graph[i]['true'] = i + 1 if (i + 1 <= size) else None
+#             graph[i]['false'] = ptr
+#         elif node_type == 'conditional':
+#             ptr = None
+#             for k in range(i+2, size):
+#                 if graph[i].get('level') == graph[k].get('level'):
+#                     ptr = k
+#                     break
+#             graph[i]['true'] = i + 1 if (i + 1 <= size) else None
+#             graph[i]['false'] = k
+#         elif node_type in ['action', 'info']:
+#             graph[i]['true'] = i + 1 if (i + 1 <= size) else None
+#             graph[i]['false'] = i + 1 if (i + 1 <= size) else None
+#
+#     return graph
 
 def main():
     results = load_results('list-of-pages-webmd-results.json')
@@ -161,10 +163,7 @@ def main():
     sample_key = "http://www.webmd.com/first-aid/fainting-treatment"
     procedure = results.get(sample_key)
 
-    parsed_procedure = parse_procedure(procedure)
-    graph = {}
-    for i, p in enumerate(parsed_procedure):
-        graph[i] = p
+    graph = parse_procedure(procedure)
 
     #graph = add_pointers(graph)
     pp = pprint.PrettyPrinter(indent=4)
